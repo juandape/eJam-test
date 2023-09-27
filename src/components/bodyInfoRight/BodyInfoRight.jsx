f25199ee16b6f7db025c09d64fab5be2e4103ee6
Juan David Peña <juandavidp76@hotmail.com>
2023-09-26 22:39:01 -0500

    perf: body right styles

--- src/components/bodyInfoRight/BodyInfoRight.jsx
+++ src/components/bodyInfoRight/BodyInfoRight.jsx
@@ -9,7 +9,7 @@ export default function BodyInfoRight() {
           only
           <span> $14 each</span> ($84.00 total!)
         </div>
-        <div>
+        <div className='container_ionizer'>
           <div>
             <img src='../../public/smallclarifion.png' alt='smallclarifion' />
           </div>
@@ -33,7 +33,82 @@ export default function BodyInfoRight() {
             Simply plug a Clarifion into any standard outlet and replace bulky,
             expensive air purifiers with a simple.
           </div>
-          
+        </div>
+        <div>
+          <div className='container_tick'>
+            <img
+              src='../../public/tick-circle.png'
+              alt='tick'
+              className='img_tick'
+            />
+            <p>
+              Negative Ion Technology may <strong>help with allergens</strong>
+            </p>
+          </div>
+          <div className='container_tick'>
+            <img
+              src='../../public/tick-circle.png'
+              alt='tick'
+              className='img_tick'
+            />
+            <p>
+              Designed for <strong>air rejuvenation</strong>
+            </p>
+          </div>
+          <div className='container_tick'>
+            <img
+              src='../../public/tick-circle.png'
+              alt='tick'
+              className='img_tick'
+            />
+            <p>
+              <strong>Perfect for every room</strong> in all types of places.
+            </p>
+          </div>
+          <div className='container_discount'>
+            <img
+              src='../../public/percentage.png'
+              alt='percentage'
+              className='img_percentage'
+            />
+            <p>
+              Save <span>53%</span> and get <span>6 extra Clarifision</span> for
+              only <span>$14 Each.</span>
+            </p>
+          </div>
+          <div className='claim'>
+            <strong>Yes - Claim my discount -&gt;</strong>
+          </div>
+          <div className='shipping'>
+            <p>Free Shipping</p>
+            <p className='stick'>|</p>
+            <img
+              src='../../public/lock-black.png'
+              alt='lock'
+              className='img_lock-black'
+            />
+            <p>Secure 256-bit SSL encryption.</p>
+            <p className='stick'>|</p>
+            <img
+              src='../../public/cards.png'
+              alt='cards'
+              className='img_cards'
+            />
+          </div>
+          <div className='dismiss'>No thanks, I don’t want this.</div>
+          <div className='satisfaction'>
+            <img
+              src='../../public/satisfaction.png'
+              alt='lock'
+              className='img_satisfaction'
+            />
+            <p>
+              If you are not completely thrilled with your Clarifion - We have a{' '}
+              <strong>30 day satisfaction guarantee</strong>. Please refer to
+              our return policy at the bottom of the page for more details.
+              Happy Shopping!
+            </p>
+          </div>
         </div>
       </div>
     </>
